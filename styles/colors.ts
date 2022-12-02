41db87e08291e61cd7fab375075402cbb58ba7f8
Andel Husbands <andelhusbands@gmail.com>
2022-12-02 15:54:45 -0400

    project setup. add ts

---
+++ styles/colors.ts
@@ -0,0 +1,11 @@
+export default {
+  primary: "#10FFC6",
+  positive: "#E9FBF0",
+  cautious: "#FFD98F",
+  copy: "#21262E",
+  secondary: "#22252D",
+  disabled: "#B1B1B1",
+  tabDefault: "#888888",
+  lightGrey: "#7b7b7b",
+  placeholder: "#B1B1B1",
+}
