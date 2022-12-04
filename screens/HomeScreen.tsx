b8a2e65cb8f0faf5b040123670215146758827ed
vindard <17693119+vindard@users.noreply.github.com>
2022-12-03 23:30:36 -0400

    chore: SoS demo to get package working

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -5,8 +5,10 @@ import { HomeScreenNavigationProp } from "../navigation/types"
 import colors from "../styles/colors"
 import { TextBold, TextLight, TextRegular } from "../styles/typography"
 import MainButton from "../styles/buttons/main-button"
-import { SQLiteDb } from "../sos/services/sqlite/get-db"
+import { SQLiteDb } from "../sqlite-helpers/get-db"
 import { useEffect } from "react"
+import { StackorSpend } from "../sos"
+
 
 const TAGGED = [
   {
@@ -25,13 +27,13 @@ const HomeScreen = () => {
   const navigation = useNavigation<HomeScreenNavigationProp>()
 
   useEffect(() => {
-    const rows = async () => {
+    const demo1 = async () => {
       const createQuery = `
         CREATE TABLE IF NOT EXISTS transactions (
           sats_amount_with_fee INTEGER NOT NULL,
           sats_fee TEXT NOT NULL
         );
-        `
+      `
 
       const insertQuery = `
         INSERT INTO transactions (
@@ -41,20 +43,84 @@ const HomeScreen = () => {
           ?,
           ?
         );
-        `
+      `
+      const dropQuery = `
+        DROP TABLE IF EXISTS transactions;
+      `
       console.log('START')
       const sql = SQLiteDb()
 
-      await sql.create({ createQuery })
+      await sql.create(createQuery)
 
       const insertRow = [300, "50"]
-      await sql.insert({insertQuery, row: insertRow})
+      await sql.insert({query: insertQuery, row: insertRow})
 
-      const rows = await sql.select({selectQuery: "SELECT * FROM transactions;"})
+      const selectQuery = `SELECT * FROM transactions;`
+      const rows = await sql.select({query: selectQuery})
       console.log('Rows:', rows)
     }
 
-    rows()
+    const demo2 = async () => {
+      console.log('START')
+      const db = SQLiteDb()
+
+      const rows = await db.select({query: `SELECT * FROM transactions;`})
+      console.log('Rows:', rows)
+    }
+
+    const demoSoS = async () => {
+      console.log('START')
+      const db = SQLiteDb()
+
+      const sos = StackorSpend({
+        galoy: {
+          endpoint: "https://api.staging.galoy.io/graphql/",
+          token: "nWL9JckgHA6uMjwuz6kkYrAowrpNXSas"
+        }
+      })
+
+      // ==========
+      // Step 1: Sync transactions from Galoy source
+      // ==========
+      console.log("Syncing transactions from Galoy...")
+      const synced = await sos.syncTxns({
+        db,
+        pageSize: 100,
+        // rescanForMissing: true,
+        // rebuild: true,
+      })
+      if (synced instanceof Error) throw synced
+      console.log("Finished sync.")
+    
+        // ==========
+        // Step 2: Retrieve synced transactions in pages
+        // ==========
+        const step2 = true
+        if (step2) {
+          console.log("Fetching transactions from local db...")
+          const pageOne = await sos.fetchTxns({ db, first: 4 })
+          if (pageOne instanceof Error) throw pageOne
+          const { cursor, txns } = pageOne
+          console.log("Page 1 txns:")
+          console.log(JSON.stringify(txns, null, 2))
+          console.log("Page 1 cursor:", cursor)
+          const pageTwo = await sos.fetchTxns({ db, first: 2, after: cursor })
+          console.log("Page 2:")
+          console.log(JSON.stringify(pageTwo, null, 2))
+        }
+
+        // ==========
+        // Step 3: Get figure for stack cost
+        // ==========
+        const step3 = true
+        if (step3) {
+          const stackCost = await sos.getStackCost(db)
+          console.log("Current (DCA'd) stack cost is:", stackCost)
+        }
+    }
+
+
+    demoSoS()
   }, [])
 
   return (
