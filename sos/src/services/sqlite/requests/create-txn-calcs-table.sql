d8b99f3fc00d2651e8091edce63189a30deae62b
vindard <17693119+vindard@users.noreply.github.com>
2022-12-05 17:09:03 -0400

    fix: 'avg_price_with_pl' can be null

--- sos/src/services/sqlite/requests/create-txn-calcs-table.sql
+++ sos/src/services/sqlite/requests/create-txn-calcs-table.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS txn_calculations (
 
     aggregate_sats INTEGER NOT NULL,
     aggregate_fiat_amount REAL NOT NULL,
-    stack_price_with_pl_included REAL NOT NULL,
+    stack_price_with_pl_included REAL,
 
     fiat_amount_less_pl REAL NOT NULL,
     fiat_pl REAL NOT NULL,
